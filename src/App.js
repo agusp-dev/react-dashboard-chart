0df1208576be73d7bf4edb2d788968e99a769581
Agustín Perez <aperez@vates.com>
2020-12-17 23:05:19 -0300

    Integrated all components and removed app logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Fragment } from 'react'
+import { Dashboard } from './components/Dashboard/Dashboard'
+import { Sidebar } from './components/Sidebar/Sidebar'
+import { Container, Box } from '@material-ui/core'
 
 function App() {
   return (
-    <div className="App">
-      Vates dashboard
-    </div>
+    <Fragment>
+      <Sidebar />
+      <Container maxWidth='xl'>
+        <Box pt={ 16 }>
+          <Dashboard />
+        </Box>
+      </Container>
+    </Fragment>
   );
 }
 
